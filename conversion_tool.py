2fab6d4f35590a215cc713cbbed62e14ec56e583
Michael Kohlhaas <michas_tor@web.de>
2020-11-04 21:06:38 +0100

    removed Non-ASCII characters

--- conversion_tool.py
+++ conversion_tool.py
@@ -1,9 +1,9 @@
 print("Wilkommen zum Konversionstool")
 
 while True:
-    print("Bitte geben Sie einen Euro-Betrag ein, den Sie in Deutsche Mark umrechnen möchten.")
+    print("Bitte geben Sie einen Euro-Betrag ein, den Sie in Deutsche Mark umrechnen moechten.")
 
-    euro = input("Betrag: €")
+    euro = input("Betrag Euro: ")
 
     euro = float(euro.replace(",", "."))
 
@@ -11,9 +11,9 @@ while True:
 
     dm = round(euro * 1.95583 ,2)
 
-    print("{0:.2f} € entsprechen {1} DM".format(euro, dm))
+    print("{0:.2f} Euro entsprechen {1:.2f} DM".format(euro, dm))
 
-    abfrage = str(input("Möchten Sie eine weitere Umrechnung durchführen lassen? (j/n): "))
+    abfrage = str(input("Moechten Sie eine weitere Umrechnung durchfuehren lassen? (j/n): "))
 
     if abfrage.lower() != "j" and abfrage.lower != "ja":
         break
